b22f3dd8931725f4447cb3074c7e851d7c57fee7
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-05 00:30:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name:, grade:, id: nil)
+  def initialize(name, grade, id: nil)
     @name, @grade, @id = name, grade, id
   end
 
